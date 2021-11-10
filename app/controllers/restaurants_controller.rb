568a8dd8a879f4e9b9a1f9aa7f65176730258f72
Mathieu Courtois <kourtois@protonmail.com>
2021-11-10 16:43:12 +0100

    review creation done

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,17 +1,48 @@
 class RestaurantsController < ApplicationController
 
+  before_action :find_restaurant, only: [:show, :edit, :update]
+
   def index
     @restaurants = Restaurant.all
   end
 
   def show
-    @restaurant = Restaurant.find(params[:id])
   end
 
   def new
+    @restaurant = Restaurant.new
+    restaurant_type
   end
 
   def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
   end
 
+  def edit
+    restaurant_type
+  end
+
+  def update
+    @restaurant.update(restaurant_params)
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
+
+  def find_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def restaurant_type
+    @restaurant_type = %w[french italian belgian chinese japanese]
+  end
 end
